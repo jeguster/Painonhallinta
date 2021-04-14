896a8890690e2be26bf1bbf4efee9e7d95c14478
jeguster <jeguster@gmail.com>
2021-04-14 13:05:36 +0300

    Noniin

--- kanta.py
+++ kanta.py
@@ -8,16 +8,15 @@ from sqlite3.dbapi2 import SQLITE_INSERT
 tietokannan_nimi = 'painonhallinta.db'
 
 def luo_tietokanta(tiedosto):
-    """Luo tietokannan huom. tiedostotyypin po. .db
-
+    """Luo tietokannan huom. tiedoston tyyppi po. .db
     Args:
-        tiedosto (string): SQLite tietokantatiedoston nimi
+        tiedosto (string): SQL Lite tietokantatiedoston nimi
     """
     yhteys = sqlite3.connect(tiedosto)
     yhteys.close()
 
 def luo_taulut(tiedosto):
-    """Luo SQLite tietokantaan tarvittavat taulut
+    """Luo SQL Lite tietokantaan tarvittavat taulut
     """
     # Muodostetaan yhteys tietokantaan, luodaan kanta tarvittaessa
     yhteys = sqlite3.connect(tiedosto)
@@ -31,36 +30,93 @@ def luo_taulut(tiedosto):
         spaiva DATE NOT NULL);''')
 
     # Luodaan Mittaukset-taulu
-    yhteys.execute('''CREATE TABLE mittaus
-        (mittaus_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    yhteys.execute('''CREATE TABLE mittaus 
+        (mittaus_id INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL,
         henkilo_id INTEGER NOT NULL,
         pituus REAL NOT NULL,
         paino REAL NOT NULL,
         FOREIGN KEY (henkilo_id)
             REFERENCES henkilo (henkilo_id)
             ON DELETE CASCADE);''')
-
+            
+    
     # Suljetaan tietokantayhteys taulujen luonnin jälkeen
     yhteys.close()
 
-# Luodaan testidataan
+# Luodaan testidataa
 def lisaa_henkilo(tiedosto, etunimi, sukunimi, sukupuoli, spaiva):
+    """Lisätään argumenttina annetun tietokannan henkilo-tauluun uusi tietue
+    Args:
+        tiedosto (string): tietokantatiedoston nimi
+        etunimi (string): Henkilön etunimi
+        sukunimi (string): Henkilön sukunimi
+        sukupuoli (int): Sukupuolikoodi 1: mies 0: nainen
+        spaiva (string): ISO-standardin mukainen päiväys YYYY-MM-DD
+    """
     # Rakennetaan SQL-lause argumenttien arvoista
-    sql_lause = "INSERT INTO henkilo (etunimi, sukunimi, sukupuoli, spaiva) VALUES (" + "'" + etunimi + "', " + "'" + sukunimi + "', " + sukupuoli + ", " + "'" + spaiva + "');"
+    sql_lause = "INSERT INTO henkilo (etunimi, sukunimi, sukupuoli, spaiva) VALUES (" + "'" + etunimi + "', " + "'" + sukunimi + "', " + str(sukupuoli) + ", " + "'" + spaiva + "');"
 
-
+    # Luodaan yhteys tietokantaan
     yhteys = sqlite3.connect(tiedosto)
 
-    yhteys.execute()
+    # Suoritetaan tietueen lisäys SQL-lauseena
+    yhteys.execute(sql_lause)
 
-# TODO: luo rutiini tietojen lukemiseksi molemmista tauluista
+    # Vahvistetaan tapahtuma (transaktio)
+    yhteys.commit()
+
+    # Suljetaan yheys
+    yhteys.close()
+
+# TODO: luo funktio, jolla saadaan puolilainausmerkit merkkijonon ympärille
+def sql_string(kentta):
+    kentta = "'" + kentta +"'"
+    return kentta
+
+# TODO: luo rutiini mittaustietojen syöttämiseksi mittaukset tauluun
+def lisaa_mittaus(tiedosto, henkilo_id, pituus, paino):
+    """Lisää henkilön mittaustiedot mittaus-tauluuun
+    Args:
+        henkilo_id (integer): henkiön id
+        pituus (float): henkilön pituus sentteinä
+        paino (float): henkilön paino kiloina
+    """
+    sql_lause = "INSERT INTO mittaus (henkilo_id, pituus, paino) VALUES (" + str(henkilo_id) + "," + str(pituus) + "," + str(paino) + ");"
+
+     # Luodaan yhteys tietokantaan
+    yhteys = sqlite3.connect(tiedosto)
+
+    # Suoritetaan tietueen lisäys SQL-lauseena
+    yhteys.execute(sql_lause)
+
+    # Vahvistetaan tapahtuma (transaktio)
+    yhteys.commit()
+
+    # Suljetaan yheys
+    yhteys.close()
+# TODO: luo rutiini tietojen lukemiseksi molemmista tauluita
 
 # Paikallinen testaus
 if __name__ == "__main__":
     # luo_tietokanta(tietokannan_nimi)
     # luo_taulut(tietokannan_nimi)
-    etunimi = 'Jorma'
-    sukunimi = 'Dorferson'
+
+    '''
+    etunimi = 'Mikko'
+    sukunimi = 'Viljanen'
     sukupuoli = 1
     spaiva = '1968-12-03'
-    sql_lause = "INSERT INTO henkilo (etunimi, sukunimi, sukupuoli, spaiva) VALUES (" + "'" + etunimi + "', " + "'" + sukunimi + "', " + sukupuoli + ", " + "'" + spaiva + "');"
+    sql_lause = "INSERT INTO henkilo (etunimi, sukunimi, sukupuoli, spaiva) VALUES (" + "'" + etunimi + "', " + "'" + sukunimi + "', " + str(sukupuoli) + ", " + "'" + spaiva + "');"
+    print(sql_lause) '''
+
+    # lisaa_henkilo(tietokannan_nimi, 'Mikko', 'Viljanen', 1, '1968-12-03')
+    # lisaa_henkilo(tietokannan_nimi, 'Mika', 'Vainio', 1, '1962-06-26')
+
+    '''
+    henkilo_id = 1
+    pituus = 171
+    paino = 74
+    sql_lause = "INSERT INTO mittaus (henkilo_id, pituus, paino) VALUES (" + str(henkilo_id) + "," + str(pituus) + "," + str(paino) + ");"
+    print(sql_lause) '''
+
+    # lisaa_mittaus(tietokannan_nimi, 2, 171, 74)
